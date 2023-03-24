95463cdf623aaa59bf4da125d8769e10af8e1d5f
Joseph Matheus <josephmatheus@hotmail.com>
2023-03-24 20:13:42 -0300

    navegação com rotas e página content criada

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Título noticia 01",
+        "description": "descrição 01",
+        "photoCover": "https://placedog.net/502"
+    },
+    {
+        "id":"2",
+        "title":"Título noticia 02",
+        "description": "descrição 02",
+        "photoCover": "https://placedog.net/601"
+    },
+    {
+        "id":"3",
+        "title":"Título noticia 03",
+        "description": "descrição 03",
+        "photoCover": "https://placedog.net/602"
+    },
+    {
+        "id":"4",
+        "title":"Título noticia 04",
+        "description": "descrição 04",
+        "photoCover": "https://placedog.net/604"
+    },
+]
