08229afa9bfb0fa48b1bd9cdaeed4e20c830b24e
Joseph Matheus <josephmatheus@hotmail.com>
2023-03-24 17:57:21 -0300

    Torna os componentes de card dinâmicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  selector: "app-big-card",
+  templateUrl: "./big-card.component.html",
+  styleUrls: ["./big-card.component.css"],
 })
 export class BigCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover: string = "";
+  @Input()
+  cardTitle: string = "";
+  @Input()
+  cardDescription: string = "";
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
